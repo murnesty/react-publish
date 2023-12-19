eadb3239c29930dc44a6d6501163b6d0e4f068ce
murnesty <murnesty@gmail.com>
2023-12-19 16:03:08 +0800

    add bootstrap library

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "popper.js";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Includes Popper.js
+import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "gantt-task-react/dist/index.css";
 
